c38618ff21bcd871dd377c4c0cd284b835323cde
Prachi <145285823+prachi0222@users.noreply.github.com>
2025-05-23 10:03:44 +0000

    datatypesummary

---
+++ 01_basics/datatypes_summery.js
@@ -0,0 +1,28 @@
+//primitive datatypes
+//7 types : String, Number, Boolean,Null, undefined, BigInt,Symbol
+
+const score=100;
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id =Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId);
+
+//Reference type(non primitive)
+//Array,Objects,Functions
+
+const heroes=["saktiman", "naagraj", "doga"]
+let myObj="hitesh",
+age=33
+
+const myfunction=function(){
+console.log("hello world");
+
+}
+console.log(typeof myfunction);
+
+      
