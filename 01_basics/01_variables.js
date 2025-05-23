63278cb439a6ff5961fc00ddb786cd926b2c7889
Prachi <145285823+prachi0222@users.noreply.github.com>
2025-05-23 06:05:15 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=1234;
+let accountEmail="prachi@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+//accountId=47668 // not allowed
+ accountEmail="pr@gmail.com"
+ accountPassword="4567"
+ accountCity="bangluru"
+
+  let accountstate; //ans is undefined
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate]);
+/**prefer not to use var
+ * because of issue in block scope and functional scope
+ */
