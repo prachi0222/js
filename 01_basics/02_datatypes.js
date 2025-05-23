57809fec7a3dd51ec410805fb6c97242b9ab1017
Prachi <145285823+prachi0222@users.noreply.github.com>
2025-05-23 08:27:47 +0000

    code config

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//teat all js code as newer version
+//alert(3+3); // we are using nodejs, not browser
+console.log(3+3); console.log("hi");//code readability should be high
+
+console.log("hitesh");
+
+let name="prachi"
+let age=18
+let isLoggedIn=false
+let state;
+
+//number=>2 to power 53
+//bigInt
+//string =>""
+//boolean=> true/false
+//null => standalon value
+//undefined=>
+//symbol => unique
+
+//object
+console.log(typeof undefined);// output: undefined
+console.log(typeof null); // output: object
+
+
+
