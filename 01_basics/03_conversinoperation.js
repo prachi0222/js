a457b3a4fee859351ba9d11b83703dd60c1d009a
Prachi <145285823+prachi0222@users.noreply.github.com>
2025-05-23 08:39:27 +0000

    code config

--- 01_basics/03_conversinoperation.js
+++ 01_basics/03_conversinoperation.js
@@ -10,6 +10,13 @@ let score="33abc"
 //"33"=>33
 //"33abc"=>NaN
 //true=>1;false=>0
-let isLoggedIn=1
+//"prachi"=>true
+let isLoggedIn=""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
