5a240145eea8421362793e6ae2252db3c25d8bb2
Prachi <145285823+prachi0222@users.noreply.github.com>
2025-05-24 12:51:04 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,3 +35,32 @@ function loginUser(username="sam") {
 console.log(loginUser())
 
 
+function calculateCartPrice(val1,val2, ...num1) {
+    return num1
+    
+}
+//console.log(calculateCartPrice(200,400,500));
+const user={
+    username:"hitesh",
+    price:199
+}
+function handleobject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+//handleobject(user)
+handleobject({
+    username:"sam",
+    price:399
+})
+
+const myNewArray=[200,400,100,600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000]));
+
+
+
+
