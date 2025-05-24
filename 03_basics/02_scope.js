fc6018534abe9ae27125a9dc297cafafdca13b1d
Prachi <145285823+prachi0222@users.noreply.github.com>
2025-05-24 17:43:22 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -6,16 +6,53 @@ if(true){
 let a=10
 const b=20
 var c=30
-console.log("INNER: ", a);
+//console.log("INNER: ", a);
 
 }
-for(let i=0; i < array.length; i++){
- const element = array[i];
-}
 
-console.log(a);
+
+////console.log(a);
 //console.log(b);
-console.log(c);
+////console.log(c);
+
+function one(){
+   // const usename ="hitesh"
+    function two() {
+      //  const website = "youtube"
+       // console.log(username);
+        
+        
+    }
+    //console.log(website);
+    two()
+}
+one()
+
+if(true){
+    const username="hitesh"
+    if(username==="hitesh"){
+       const website="youtube"
+       //console.log(username+website);
+       
+    }
+    //console.log(website);
+    
+}
+//console.log(username);
+
+//++++++++++++++++++++++++++ intresting ++++++++++++++++
+
+function addone(num){
+
+   return num + 1
+}
+addone(5)
+addtwo(5)
+const addtwo= function(num){
+
+   return num + 2
+}
+
 
 
 
